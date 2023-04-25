90c48f29d3efe1efcae53a6e9f114d0cb5babe8e
dragonfighterss <dragonfighter0111@outlook.com>
2023-04-25 12:14:46 -0400

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/payment.controller');
-//sfsdfssfsfsdfsdfsdsfsfsdfsfsds
+//sfsdfssfsfsdfsdfsdsfsfsdfsfsdssfs
 router.get('/',  		paymentController.index)
 router.get('/redirect', paymentController.redirect)
 
